5e15a7f4fd3aa87c26716713a67a014a5f1f4ad6
Nils Breer <nils.breer@tu-dortmund.de>
2023-02-19 14:51:32 +0100

    added reload speed to attributes

--- gui_buffs_tkinter.py
+++ gui_buffs_tkinter.py
@@ -7,11 +7,8 @@ from tkinter import *
 #import mysql.connector
 
 class Window():
-    def __init__(self, master, LFR_reload_speed, Rocket_reload_speed, GL_reload_speed):
+    def __init__(self, master):
         self.master = master
-        self.LFR_reload_speed = LFR_reload_speed
-        self.Rocket_reload_speed = Rocket_reload_speed
-        self.GL_reload_speed = GL_reload_speed
         self.Main = Frame(self.master)  # ---> define main
 
         multiplier = [1]
@@ -324,7 +321,7 @@ def LFR_dps():
     if window.S0.get() == 0:
         return 0, 0
     else:
-        return round(window.S0.get() * wp_attributes[Svar0.get()]['mag_size'] * float(base_LFR_dmg) / (window.S0.get() * wp_attributes[Svar0.get()]['mag_size'] * ((wp_attributes[Svar0.get()]['charge_time'] * 1e-3) + 0.44) + (window.S0.get() - 1) * window.LFR_reload_speed), 1), round(window.S0.get() * wp_attributes[Svar0.get()]['mag_size'] * float(base_LFR_dmg_3burst) / (window.S0.get() * wp_attributes[Svar0.get()]['mag_size'] * ((wp_attributes[Svar0.get()]['charge_time'] * 1e-3) + 0.44) + (window.S0.get() - 1) * window.LFR_reload_speed), 1)
+        return round(window.S0.get() * wp_attributes[Svar0.get()]['mag_size'] * float(base_LFR_dmg) / (window.S0.get() * wp_attributes[Svar0.get()]['mag_size'] * ((wp_attributes[Svar0.get()]['charge_time'] * 1e-3) + 0.44) + (window.S0.get() - 1) * wp_attributes[Svar0.get()]['reload_speed']), 1), round(window.S0.get() * wp_attributes[Svar0.get()]['mag_size'] * float(base_LFR_dmg_3burst) / (window.S0.get() * wp_attributes[Svar0.get()]['mag_size'] * ((wp_attributes[Svar0.get()]['charge_time'] * 1e-3) + 0.44) + (window.S0.get() - 1) * wp_attributes[Svar0.get()]['reload_speed']), 1)
 
 def rocket_dps():
     base_rocket_dmg = window.L7.cget("text")
@@ -333,7 +330,7 @@ def rocket_dps():
     elif window.S1.get() == 1:
         return round(float(base_rocket_dmg), 1)
     else:
-        return round((window.S1.get() * float(base_rocket_dmg)) / ((window.S1.get() - 1) * window.Rocket_reload_speed), 1)
+        return round((window.S1.get() * float(base_rocket_dmg)) / ((window.S1.get() - 1) * wp_attributes[Svar0.get()]['reload_speed']), 1)
 
 def GL_dps():
     base_GL_dmg = window.L7.cget("text")
@@ -341,7 +338,7 @@ def GL_dps():
     if window.S2.get() == 0:
         return 0
     else:
-        return round((window.S2.get() * float(base_GL_dmg) * wp_attributes[Svar0.get()]['mag_size']) / (window.S2.get() * time_for_mag + (window.S2.get() - 1) * window.GL_reload_speed), 1)
+        return round((window.S2.get() * float(base_GL_dmg) * wp_attributes[Svar0.get()]['mag_size']) / (window.S2.get() * time_for_mag + (window.S2.get() - 1) * wp_attributes[Svar0.get()]['reload_speed']), 1)
 
 def Select_Weapon():
     undo()
@@ -596,47 +593,47 @@ Cvar20 = IntVar() # lasting impression
 
 # here: add ALL weapons for the dropdown menu
 wp_attributes = {"Cataclysmic":
-        {"charge_time": 533, "base_dmg": 56586, "mag_size": 6, "reserves": 22},
+        {"charge_time": 533, "base_dmg": 56586, "mag_size": 6, "reserves": 22, "reload_speed": 3.69},
     "Stormchaser":
-        {"charge_time": 533, "base_dmg": 27704, "mag_size": 5, "reserves": 22},
+        {"charge_time": 533, "base_dmg": 27704, "mag_size": 5, "reserves": 22, "reload_speed": 3.69},
     "Fire and Forget":
-        {"charge_time": 533, "base_dmg": 28258, "mag_size": 5, "reserves": 22},
+        {"charge_time": 533, "base_dmg": 28258, "mag_size": 5, "reserves": 22, "reload_speed": 3.74},
     "Reed's Regret":
-        {"charge_time": 533, "base_dmg": 57822, "mag_size": 5, "reserves": 22},
+        {"charge_time": 533, "base_dmg": 57822, "mag_size": 5, "reserves": 22, "reload_speed": 3.59},
     "Sailspy Pitchglass":
-        {"charge_time": 533, "base_dmg": 58356, "mag_size": 5, "reserves": 22},
+        {"charge_time": 533, "base_dmg": 58356, "mag_size": 5, "reserves": 22, "reload_speed": 3.73},
     "Taipan 4FR":
-        {"charge_time": 533, "base_dmg": 58356, "mag_size": 5, "reserves": 22},
+        {"charge_time": 533, "base_dmg": 58356, "mag_size": 5, "reserves": 22, "reload_speed": 3.63},
     "Threaded Needle":
-        {"charge_time": 533, "base_dmg": 58356, "mag_size": 5, "reserves": 22},
+        {"charge_time": 533, "base_dmg": 58356, "mag_size": 5, "reserves": 22, "reload_speed": 3.78},
     "The Hothead":
-        {"rpm": 20, "base_dmg": 98208, "mag_size": 1, "reserves": 7},
+        {"rpm": 20, "base_dmg": 98208, "mag_size": 1, "reserves": 7, "reload_speed": 3.12},
     "Blowout":
-        {"rpm": 20, "base_dmg": 98208, "mag_size": 1, "reserves": 7},
+        {"rpm": 20, "base_dmg": 98208, "mag_size": 1, "reserves": 7, "reload_speed": 3.12},
     "Roar Of The Bear":
-        {"rpm": 15, "base_dmg": 84309, "mag_size": 1, "reserves": 7},
+        {"rpm": 15, "base_dmg": 84309, "mag_size": 1, "reserves": 7, "reload_speed": 3.35},
     "Hezen Vengeance":
-        {"rpm": 25, "base_dmg": 98209, "mag_size": 1, "reserves": 7},
+        {"rpm": 25, "base_dmg": 98209, "mag_size": 1, "reserves": 7, "reload_speed": 2.92},
     "Code Duello":
-        {"rpm": 15, "base_dmg": 84309, "mag_size": 1, "reserves": 7},
+        {"rpm": 15, "base_dmg": 84309, "mag_size": 1, "reserves": 7, "reload_speed": 3.34},
     "RedHerring":
-        {"rpm": 20, "base_dmg": 98208, "mag_size": 1, "reserves": 7},
+        {"rpm": 20, "base_dmg": 98208, "mag_size": 1, "reserves": 7, "reload_speed": 3.22},
     "Royal Entry":
-        {"rpm": 15, "base_dmg": 80352, "mag_size": 1, "reserves": 7},
+        {"rpm": 15, "base_dmg": 80352, "mag_size": 1, "reserves": 7, "reload_speed": 3.45},
     "Bump In The Night":
-        {"rpm": 25, "base_dmg": 98209, "mag_size": 1, "reserves": 7},
+        {"rpm": 25, "base_dmg": 98209, "mag_size": 1, "reserves": 7, "reload_speed": 2.98},
     "Palmyra-B":
-        {"rpm": 15, "base_dmg": 80352, "mag_size": 1, "reserves": 7},
+        {"rpm": 15, "base_dmg": 80352, "mag_size": 1, "reserves": 7, "reload_speed": 3.59},
     "Wendigo GL3":
-        {"rpm": 120, "base_dmg": 39531, "mag_size": 6, "reserves": 1},
+        {"rpm": 120, "base_dmg": 39531, "mag_size": 6, "reserves": 1, "reload_speed": 3.14},
     "Interference VI":
-        {"rpm": 120, "base_dmg": 39382, "mag_size": 6, "reserves": 1},
+        {"rpm": 120, "base_dmg": 39382, "mag_size": 6, "reserves": 1, "reload_speed": 3.25},
     "Tarnation":
-        {"rpm": 150, "base_dmg": 31880, "mag_size": 5, "reserves": 1},
+        {"rpm": 150, "base_dmg": 31880, "mag_size": 5, "reserves": 1, "reload_speed": 3.57},
     "Cry Mutiny":
-        {"rpm": 120, "base_dmg": 35411, "mag_size": 4, "reserves": 1},
+        {"rpm": 120, "base_dmg": 35411, "mag_size": 4, "reserves": 1, "reload_speed": 3.16},
     "Typhon GL5":
-        {"rpm": 120, "base_dmg": 39155, "mag_size": 6, "reserves": 1},
+        {"rpm": 120, "base_dmg": 39155, "mag_size": 6, "reserves": 1, "reload_speed": 3.16},
 }
 
 weapon_list = list(wp_attributes.keys())
@@ -650,7 +647,7 @@ Svar0.set(list(wp_attributes.keys())[0])
 Rvar0 = IntVar()
 Rvar1 = IntVar()
 
-window = Window(root, 3.6, 3.0, 3.14) # unit for reload speed is seconds
+window = Window(root) # unit for reload speed is seconds
 
 root.mainloop()
 

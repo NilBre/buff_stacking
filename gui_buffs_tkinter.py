738a58cc961a43b270c7177f9a92cc5cf1556398
Nils Breer <nils.breer@tu-dortmund.de>
2023-03-24 13:33:12 +0100

    added weapon catalog in seperate file for easier handling wrote season power changes in different file, still need to be added in production

--- gui_buffs_tkinter.py
+++ gui_buffs_tkinter.py
@@ -6,6 +6,7 @@ import re
 # import tkinter as ttk
 from tkinter import *
 #import mysql.connector
+from weapon_catalog import wp_attributes
 
 class Window():
     def __init__(self, master):
@@ -209,10 +210,10 @@ class Window():
         self.canvas = FigureCanvasTkAgg(fig, master=self.Main)
         self.canvas.get_tk_widget().pack()
 
-        self.B10 = Button(self.Main, text="plot graph", command=plot)
+        self.B10 = Button(self.section0, text="plot graph", command=plot)
         self.B10.pack(pady=4)
 
-        self.B11 = Button(self.Main, text="clear graphs", command=clear_plot)
+        self.B11 = Button(self.section0, text="clear graphs", command=clear_plot)
         self.B11.pack(pady=4)
 
         self.Main.pack(padx=4, pady=4)
@@ -697,49 +698,49 @@ Cvar19 = IntVar() # Adagio
 Cvar20 = IntVar() # lasting impression
 
 # here: add ALL weapons for the dropdown menu
-wp_attributes = {"Cataclysmic":
-        {"charge_time": 533, "base_dmg": 56586, "mag_size": 6, "reserves": 22, "reload_speed": 3.69},
-    "Stormchaser":
-        {"charge_time": 533, "base_dmg": 27704, "mag_size": 5, "reserves": 22, "reload_speed": 3.69},
-    "Fire and Forget":
-        {"charge_time": 533, "base_dmg": 28258, "mag_size": 5, "reserves": 22, "reload_speed": 3.74},
-    "Reed's Regret":
-        {"charge_time": 533, "base_dmg": 57822, "mag_size": 5, "reserves": 22, "reload_speed": 3.59},
-    "Sailspy Pitchglass":
-        {"charge_time": 533, "base_dmg": 58356, "mag_size": 5, "reserves": 22, "reload_speed": 3.73},
-    "Taipan 4FR":
-        {"charge_time": 533, "base_dmg": 58356, "mag_size": 5, "reserves": 22, "reload_speed": 3.63},
-    "Threaded Needle":
-        {"charge_time": 533, "base_dmg": 58356, "mag_size": 5, "reserves": 22, "reload_speed": 3.78},
-    "The Hothead":
-        {"rpm": 20, "base_dmg": 98208, "mag_size": 1, "reserves": 7, "reload_speed": 3.12},
-    "Blowout":
-        {"rpm": 20, "base_dmg": 98208, "mag_size": 1, "reserves": 7, "reload_speed": 3.12},
-    "Roar Of The Bear":
-        {"rpm": 15, "base_dmg": 84309, "mag_size": 1, "reserves": 7, "reload_speed": 3.35},
-    "Hezen Vengeance":
-        {"rpm": 25, "base_dmg": 98209, "mag_size": 1, "reserves": 7, "reload_speed": 2.92},
-    "Code Duello":
-        {"rpm": 15, "base_dmg": 84309, "mag_size": 1, "reserves": 7, "reload_speed": 3.34},
-    "RedHerring":
-        {"rpm": 20, "base_dmg": 98208, "mag_size": 1, "reserves": 7, "reload_speed": 3.22},
-    "Royal Entry":
-        {"rpm": 15, "base_dmg": 80352, "mag_size": 1, "reserves": 7, "reload_speed": 3.45},
-    "Bump In The Night":
-        {"rpm": 25, "base_dmg": 98209, "mag_size": 1, "reserves": 7, "reload_speed": 2.98},
-    "Palmyra-B":
-        {"rpm": 15, "base_dmg": 80352, "mag_size": 1, "reserves": 7, "reload_speed": 3.59},
-    "Wendigo GL3":
-        {"rpm": 120, "base_dmg": 39531, "mag_size": 6, "reserves": 24, "reload_speed": 3.14},
-    "Interference VI":
-        {"rpm": 120, "base_dmg": 39382, "mag_size": 6, "reserves": 24, "reload_speed": 3.25},
-    "Tarnation":
-        {"rpm": 150, "base_dmg": 31880, "mag_size": 5, "reserves": 24, "reload_speed": 3.57},
-    "Cry Mutiny":
-        {"rpm": 120, "base_dmg": 35411, "mag_size": 4, "reserves": 24, "reload_speed": 3.16},
-    "Typhon GL5":
-        {"rpm": 120, "base_dmg": 39155, "mag_size": 6, "reserves": 24, "reload_speed": 3.16},
-}
+#wp_attributes = {"Cataclysmic":
+#        {"charge_time": 533, "base_dmg": 56586, "mag_size": 6, "reserves": 22, "reload_speed": 3.69},
+#    "Stormchaser":
+#        {"charge_time": 533, "base_dmg": 27704, "mag_size": 5, "reserves": 22, "reload_speed": 3.69},
+#    "Fire and Forget":
+#        {"charge_time": 533, "base_dmg": 28258, "mag_size": 5, "reserves": 22, "reload_speed": 3.74},
+#    "Reed's Regret":
+#        {"charge_time": 533, "base_dmg": 57822, "mag_size": 5, "reserves": 22, "reload_speed": 3.59},
+#    "Sailspy Pitchglass":
+#        {"charge_time": 533, "base_dmg": 58356, "mag_size": 5, "reserves": 22, "reload_speed": 3.73},
+#    "Taipan 4FR":
+#        {"charge_time": 533, "base_dmg": 58356, "mag_size": 5, "reserves": 22, "reload_speed": 3.63},
+#    "Threaded Needle":
+#        {"charge_time": 533, "base_dmg": 58356, "mag_size": 5, "reserves": 22, "reload_speed": 3.78},
+#    "The Hothead":
+#        {"rpm": 20, "base_dmg": 98208, "mag_size": 1, "reserves": 7, "reload_speed": 3.12},
+#    "Blowout":
+#        {"rpm": 20, "base_dmg": 98208, "mag_size": 1, "reserves": 7, "reload_speed": 3.12},
+#    "Roar Of The Bear":
+#        {"rpm": 15, "base_dmg": 84309, "mag_size": 1, "reserves": 7, "reload_speed": 3.35},
+#    "Hezen Vengeance":
+#        {"rpm": 25, "base_dmg": 98209, "mag_size": 1, "reserves": 7, "reload_speed": 2.92},
+#    "Code Duello":
+#        {"rpm": 15, "base_dmg": 84309, "mag_size": 1, "reserves": 7, "reload_speed": 3.34},
+#    "RedHerring":
+#        {"rpm": 20, "base_dmg": 98208, "mag_size": 1, "reserves": 7, "reload_speed": 3.22},
+#    "Royal Entry":
+#        {"rpm": 15, "base_dmg": 80352, "mag_size": 1, "reserves": 7, "reload_speed": 3.45},
+#    "Bump In The Night":
+#        {"rpm": 25, "base_dmg": 98209, "mag_size": 1, "reserves": 7, "reload_speed": 2.98},
+#    "Palmyra-B":
+#        {"rpm": 15, "base_dmg": 80352, "mag_size": 1, "reserves": 7, "reload_speed": 3.59},
+#    "Wendigo GL3":
+#        {"rpm": 120, "base_dmg": 39531, "mag_size": 6, "reserves": 24, "reload_speed": 3.14},
+#   "Interference VI":
+#        {"rpm": 120, "base_dmg": 39382, "mag_size": 6, "reserves": 24, "reload_speed": 3.25},
+#    "Tarnation":
+#        {"rpm": 150, "base_dmg": 31880, "mag_size": 5, "reserves": 24, "reload_speed": 3.57},
+#    "Cry Mutiny":
+#        {"rpm": 120, "base_dmg": 35411, "mag_size": 4, "reserves": 24, "reload_speed": 3.16},
+#    "Typhon GL5":
+#        {"rpm": 120, "base_dmg": 39155, "mag_size": 6, "reserves": 24, "reload_speed": 3.16},
+#}
 
 N_LFRs = 7
 N_Rockets = 9
---
+++ weapon_catalog.py
@@ -0,0 +1,43 @@
+wp_attributes = {"Cataclysmic":
+        {"charge_time": 533, "base_dmg": 56586, "mag_size": 6, "reserves": 22, "reload_speed": 3.69},
+    "Stormchaser":
+        {"charge_time": 533, "base_dmg": 27704, "mag_size": 5, "reserves": 22, "reload_speed": 3.69},
+    "Fire and Forget":
+        {"charge_time": 533, "base_dmg": 28258, "mag_size": 5, "reserves": 22, "reload_speed": 3.74},
+    "Reed's Regret":
+        {"charge_time": 533, "base_dmg": 57822, "mag_size": 5, "reserves": 22, "reload_speed": 3.59},
+    "Sailspy Pitchglass":
+        {"charge_time": 533, "base_dmg": 58356, "mag_size": 5, "reserves": 22, "reload_speed": 3.73},
+    "Taipan 4FR":
+        {"charge_time": 533, "base_dmg": 58356, "mag_size": 5, "reserves": 22, "reload_speed": 3.63},
+    "Threaded Needle":
+        {"charge_time": 533, "base_dmg": 58356, "mag_size": 5, "reserves": 22, "reload_speed": 3.78},
+    "The Hothead":
+        {"rpm": 20, "base_dmg": 98208, "mag_size": 1, "reserves": 7, "reload_speed": 3.12},
+    "Blowout":
+        {"rpm": 20, "base_dmg": 98208, "mag_size": 1, "reserves": 7, "reload_speed": 3.12},
+    "Roar Of The Bear":
+        {"rpm": 15, "base_dmg": 84309, "mag_size": 1, "reserves": 7, "reload_speed": 3.35},
+    "Hezen Vengeance":
+        {"rpm": 25, "base_dmg": 98209, "mag_size": 1, "reserves": 7, "reload_speed": 2.92},
+    "Code Duello":
+        {"rpm": 15, "base_dmg": 84309, "mag_size": 1, "reserves": 7, "reload_speed": 3.34},
+    "RedHerring":
+        {"rpm": 20, "base_dmg": 98208, "mag_size": 1, "reserves": 7, "reload_speed": 3.22},
+    "Royal Entry":
+        {"rpm": 15, "base_dmg": 80352, "mag_size": 1, "reserves": 7, "reload_speed": 3.45},
+    "Bump In The Night":
+        {"rpm": 25, "base_dmg": 98209, "mag_size": 1, "reserves": 7, "reload_speed": 2.98},
+    "Palmyra-B":
+        {"rpm": 15, "base_dmg": 80352, "mag_size": 1, "reserves": 7, "reload_speed": 3.59},
+    "Wendigo GL3":
+        {"rpm": 120, "base_dmg": 39531, "mag_size": 6, "reserves": 24, "reload_speed": 3.14},
+    "Interference VI":
+        {"rpm": 120, "base_dmg": 39382, "mag_size": 6, "reserves": 24, "reload_speed": 3.25},
+    "Tarnation":
+        {"rpm": 150, "base_dmg": 31880, "mag_size": 5, "reserves": 24, "reload_speed": 3.57},
+    "Cry Mutiny":
+        {"rpm": 120, "base_dmg": 35411, "mag_size": 4, "reserves": 24, "reload_speed": 3.16},
+    "Typhon GL5":
+        {"rpm": 120, "base_dmg": 39155, "mag_size": 6, "reserves": 24, "reload_speed": 3.16},
+}
